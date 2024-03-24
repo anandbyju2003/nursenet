2ebd75b23642193b831a7e57543897e54160bf84
Jamesupback <jamessajan07@gmail.com>
2024-03-25 00:20:25 +0530

    edit on css file type

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(session({
 }))
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(process.env['URI']).then(()=>console.log("db connected"))
