700cd13eed4d78e589174ebde857485db528dd08
Jamesupback <jamessajan07@gmail.com>
2024-03-24 23:11:01 +0530

    hosted db on cloud

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const session=require('express-session');
 const mongoose=require('mongoose');
+const {mongoClient}=require('mongodb');
 const bodyParser=require('body-parser');
 const {Users,Workers,Bookings}=require('./schema');
 const app=express();
@@ -14,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/test').then(()=>console.log("db connected"))
+mongoose.connect(process.env['URI']).then(()=>console.log("db connected"))
 app.route('/')
     .get((req, res) => {
         res.sendFile(__dirname + '/public/index.html');
@@ -24,10 +26,10 @@ app.route('/usersignup')
     .get((req, res) => {
         res.sendFile(__dirname+'/public/usersignup.html')
     })
-    .post((req, res) => {
+    .post(async(req, res) => {
         const {name,email,address,city,pincode,contact,password} = req.body;
         const user = new Users({name:name, email:email, address:address, city:city, pincode:pincode, contact:contact,password:password });
-        user.save().then(() => {
+        await user.save().then(() => {
             res.send('Data saved');
         });
     });
@@ -36,9 +38,9 @@ app.route("/userlogin")
     .get((req,res)=>{
         res.sendFile(__dirname+'/public/userlogin.html');
     })
-    .post((req,res)=>{
+    .post(async(req,res)=>{
         const {email,password}=req.body;
-        Users.findOne({email:email,password:password}).then((data)=>{
+        await Users.findOne({email:email,password:password}).then((data)=>{
             if(data){
                 req.session.myid=data.id;
                 res.sendFile(__dirname+'/public/userhome.html');
@@ -48,222 +50,225 @@ app.route("/userlogin")
             }
         })});
 app.route('/workersignup')
-        .get((req,res)=>{
-            res.sendFile(__dirname+'/public/workersignup.html');
-        })
-        .post((req,res)=>{
-            const {name,description,email,address,city,pincode,contact,profession,password}=req.body;
-            const worker=new Workers({name,description,email,address,city,pincode,contact,profession,password});
-            worker.save().then(()=>{
-                res.send('Data saved');
-            });
-        });
-app.route("/workerlogin")
-    .get((req,res)=>{
-        res.sendFile(__dirname+'/public/workerlogin.html');
+    .get((req, res) => {
+        res.sendFile(__dirname + '/public/workersignup.html');
     })
-    .post((req,res)=>{
-        const {email,password}=req.body;
-        Workers.findOne({email:email,password:password}).then((data)=>{
-            if(data){
-                req.session.workmyid=data.id;
-                Bookings.find({workerid:data.id,bookingstatus: { $in: ["pending", "accepted"] }})
+    .post(async (req, res) => {
+        const { name, description, email, address, city, pincode, contact, profession, password } = req.body;
+        const worker = new Workers({ name, description, email, address, city, pincode, contact, profession, password });
+        await worker.save().then(() => {
+            res.send('Data saved');
+        });
+    });
+
+app.route("/workerlogin")
+    .get((req, res) => {
+        res.sendFile(__dirname + '/public/workerlogin.html');
+    })
+    .post(async (req, res) => {
+        const { email, password } = req.body;
+        await Workers.findOne({ email: email, password: password }).then(async(data) => {
+            if (data) {
+                req.session.workmyid = data.id;
+                await Bookings.find({ workerid: data.id, bookingstatus: { $in: ["pending", "accepted"] } })
                     .populate('userid', 'name email address city pincode contact')
-                    .then((books)=>{
-                        res.render('workerdashboard',{books,data});
+                    .then((books) => {
+                        res.render('workerdashboard', { books, data });
                     });
             }
-            else{
+            else {
                 res.send('failed to login');
             }
-        })})
+        });
+    });
 app.route("/userhome")
-        .get((req,res)=>{
-            const profession=req.query.profession;
-            Workers.find({ profession: profession}).then((data) => {
-                res.render('listworkers', { data });
-            })
-        })
+    .get(async (req, res) => {
+        const profession = req.query.profession;
+        await Workers.find({ profession: profession }).then((data) => {
+            res.render('listworkers', { data });
+        });
+    });
 
 app.route("/workerdetails")
-    .get((req,res)=>{
-        const id=req.query.id;
-        const userid=req.session.myid;
-        req.session.workerid=id;
-        Workers.findById(id).then((data)=>{
-            res.render('workerdetails',{ data , userid});
+    .get(async (req, res) => {
+        const id = req.query.id;
+        const userid = req.session.myid;
+        req.session.workerid = id;
+        await Workers.findById(id).then((data) => {
+            res.render('workerdetails', { data, userid });
         });
     });
 
 app.route("/bookings")
-    .get((req,res)=>{
-        const workerid=req.session.workerid;
-        const userid=req.session.myid; 
-        res.render('booking',{workerid,userid});
+    .get(async (req, res) => {
+        const workerid = req.session.workerid;
+        const userid = req.session.myid;
+        res.render('booking', { workerid, userid });
     });
 
-    
+
 app.route("/bookconfirm")
-    .post((req,res)=>{
-        const workerid=req.session.workerid;
-        const userid=req.session.myid; 
-        const date=req.body.date;
-        const time=req.body.time;
-        const locationLink=req.body.locationLink;
-        const problem=req.body.problem;
-        const problemStatement=req.body.problemStatement;
-        const booking=new Bookings({workerid,userid,date,time,locationLink,problem,problemStatement});
-        booking.save().then((data)=>{
+    .post(async (req, res) => {
+        const workerid = req.session.workerid;
+        const userid = req.session.myid;
+        const date = req.body.date;
+        const time = req.body.time;
+        const locationLink = req.body.locationLink;
+        const problem = req.body.problem;
+        const problemStatement = req.body.problemStatement;
+        const booking = new Bookings({ workerid, userid, date, time, locationLink, problem, problemStatement });
+        await booking.save().then((data) => {
             console.log(data);
             res.redirect('/userdashboard');
         });
     });
 
-    app.route("/userdashboard")
-    .get((req,res)=>{
-        const userid=req.session.myid;
-        Bookings.find({ userid: userid })
+app.route("/userdashboard")
+    .get(async (req, res) => {
+        const userid = req.session.myid;
+        await Bookings.find({ userid: userid })
             .populate('workerid', 'name email contact profession')
             .then((data) => {
                 res.render('userdashboard', { data });
             });
     });
 
-    app.route("/acceptbooking")
-    .post((req,res)=>{
+app.route("/acceptbooking")
+    .post(async (req, res) => {
         const bookingId = req.body.bookingid;
         const bookingStatus = req.body.bookingstatus;
 
-        Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: bookingStatus } })
+        await Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: bookingStatus } })
             .then(() => {
                 res.send('Booking status updated');
             })
             .catch((error) => {
                 console.log(error);
                 res.send('Failed to update booking status');
-            });
-    })
-
-    app.route("/cancelstatus")
-    .post((req,res)=>{
-        const bookingId = req.body.bookingid;
-        Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: "pending" } })
-            .then(() => {
-                res.send('Booking status updated');
-            })
-            .catch((error) => {
-                console.log(error);
-                res.send('Failed to update booking status');
-            });
-    })
-    app.route('/rejectbooking')
-    .post((req,res)=>{
-        const bookingId = req.body.bookingid;
-        Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: "rejected" } })
-            .then(() => {
-                res.send('Booking status updated');
-            })
-            .catch((error) => {
-                console.log(error);
-                res.send('Failed to update booking status');
-            });
-    })
-
-    app.route("/requestpayment")
-    .post((req,res)=>{
-        const bookingId = req.body.bookingid;
-        Bookings.findOneAndUpdate({ _id: bookingId }, { $set: { paymentstatus: "requested" } })
-            .then(() => {
-                res.send('Payment requested');
-            })
-            .catch((error) => {
-                console.log(error);
-                res.send('Failed to request payment');
-            });
-        })
-    app.route("/cancelbooking")
-    .post((req,res)=>{
-        const bookingId = req.body.bookingid;
-        Bookings.findByIdAndDelete(bookingId)
-            .then(() => {
-                res.send('Booking cancelled');
-            })
-            .catch((error) => {
-                console.log(error);
-                res.send('Failed to cancel booking');
-            });
-           
-        });
-        
-    app.route("/paynow")
-    .post((req,res)=>{
-        const bookingId = req.body.bookingid;
-        Bookings.findOneAndUpdate({ _id: bookingId }, { $set: { paymentstatus: "paid" } })
-            .catch((error) => {
-                console.log(error);
-                res.send('Failed to update payment status');
-            });
-    })
-
-    app.route('/userprofileedit')
-    .get((req,res)=>{
-        const userid=req.session.myid;
-        Users.findById(userid).then((data)=>{
-            res.render('userprofileedit',{data});
-        });
-    })
-    .post((req,res)=>{
-        const userid=req.session.myid;
-        const {address,city,pincode,contact,password}=req.body;
-        Users.findByIdAndUpdate(userid, { address, city, pincode, contact, password }).then(() => {
-            res.redirect('/userdashboard');
-        });
-    })
-
-    app.route('/workerprofileedit')
-    .get((req,res)=>{
-        const workerid=req.session.workmyid;
-        Workers.findById(workerid).then((data)=>{
-            res.render('workerprofileedit',{data});
-        });
-    })
-    .post((req,res)=>{
-        const workerid=req.session.workmyid;
-        const {address,city,pincode,contact,password,description}=req.body;
-        Workers.findByIdAndUpdate(workerid, { address, city, pincode, contact, password, description }).then(() => {
-            res.redirect('/workerdashboard');
-        });
-    })
-
-    app.route("/rateworker")
-    .post((req, res) => {
-        const { rating, review, bookingid } = req.body;
-        Bookings.findById(bookingid)
-            .populate('userid', 'name')
-            .then((data) => {
-                Workers.findById(data.workerid).then((worker) => {
-                    const newRating = (Number(worker.rating) + Number(rating)) / 2;
-                    const newReview = { reviewer:data.userid.name,review: review };
-                    Workers.findByIdAndUpdate(data.workerid, {
-                        noofratings: worker.noofratings + 1,
-                        rating: newRating,
-                        $push: { reviews: newReview } 
-                    }).then(() => {
-                        res.send("done");
-                    }).catch(err => {
-                        console.error("Error updating worker:", err);
-                        res.status(500).send("Error updating worker");
-                    });
-                }).catch(err => {
-                    console.error("Error finding worker:", err);
-                    res.status(500).send("Error finding worker");
-                });
-            }).catch(err => {
-                console.error("Error finding booking:", err);
-                res.status(500).send("Error finding booking");
             });
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    app.route("/cancelstatus")
+        .post(async (req, res) => {
+            const bookingId = req.body.bookingid;
+            await Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: "pending" } })
+                .then(() => {
+                    res.send('Booking status updated');
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Failed to update booking status');
+                });
+        });
+
+    app.route('/rejectbooking')
+        .post(async (req, res) => {
+            const bookingId = req.body.bookingid;
+            await Bookings.findByIdAndUpdate(bookingId, { $set: { bookingstatus: "rejected" } })
+                .then(() => {
+                    res.send('Booking status updated');
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Failed to update booking status');
+                });
+        });
+
+    app.route("/requestpayment")
+        .post(async (req, res) => {
+            const bookingId = req.body.bookingid;
+            await Bookings.findOneAndUpdate({ _id: bookingId }, { $set: { paymentstatus: "requested" } })
+                .then(() => {
+                    res.send('Payment requested');
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Failed to request payment');
+                });
+        });
+
+    app.route("/cancelbooking")
+        .post(async (req, res) => {
+            const bookingId = req.body.bookingid;
+            await Bookings.findByIdAndDelete(bookingId)
+                .then(() => {
+                    res.send('Booking cancelled');
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Failed to cancel booking');
+                });
+        });
+
+    app.route("/paynow")
+        .post(async (req, res) => {
+            const bookingId = req.body.bookingid;
+            await Bookings.findOneAndUpdate({ _id: bookingId }, { $set: { paymentstatus: "paid" } })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Failed to update payment status');
+                });
+        });
+
+    app.route('/userprofileedit')
+        .get(async (req, res) => {
+            const userid = req.session.myid;
+            await Users.findById(userid).then((data) => {
+                res.render('userprofileedit', { data });
+            });
+        })
+        .post(async (req, res) => {
+            const userid = req.session.myid;
+            const { address, city, pincode, contact, password } = req.body;
+            await Users.findByIdAndUpdate(userid, { address, city, pincode, contact, password }).then(() => {
+                res.redirect('/userdashboard');
+            });
+        });
+
+    app.route('/workerprofileedit')
+        .get(async (req, res) => {
+            const workerid = req.session.workmyid;
+            await Workers.findById(workerid).then((data) => {
+                res.render('workerprofileedit', { data });
+            });
+        })
+        .post(async (req, res) => {
+            const workerid = req.session.workmyid;
+            const { address, city, pincode, contact, password, description } = req.body;
+            await Workers.findByIdAndUpdate(workerid, { address, city, pincode, contact, password, description }).then(() => {
+                res.redirect('/workerdashboard');
+            });
+        });
+
+    app.route("/rateworker")
+        .post(async (req, res) => {
+            const { rating, review, bookingid } = req.body;
+            await Bookings.findById(bookingid)
+                .populate('userid', 'name')
+                .then(async(data) => {
+                    await Workers.findById(data.workerid).then(async(worker) => {
+                        const newRating = (Number(worker.rating) + Number(rating)) / 2;
+                        const newReview = { reviewer: data.userid.name, review: review };
+                        await Workers.findByIdAndUpdate(data.workerid, {
+                            noofratings: worker.noofratings + 1,
+                            rating: newRating,
+                            $push: { reviews: newReview }
+                        }).then(() => {
+                            res.send("done");
+                        }).catch(err => {
+                            console.error("Error updating worker:", err);
+                            res.status(500).send("Error updating worker");
+                        });
+                    }).catch(err => {
+                        console.error("Error finding worker:", err);
+                        res.status(500).send("Error finding worker");
+                    });
+                }).catch(err => {
+                    console.error("Error finding booking:", err);
+                    res.status(500).send("Error finding booking");
+                });
+        });
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
